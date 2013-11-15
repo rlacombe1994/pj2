11f0d8dc0e9e4831c3d479971cbf1648c421d1f1
mseeg <Canismajoris@gmx.com>
2013-11-15 16:27:26 -0600

    Update Priest.cpp

--- Priest.cpp
+++ Priest.cpp
@@ -2,22 +2,26 @@
 
 Priest::Priest(string name,  int attackDamage) : MilitaryUnit(name, attackDamage)
 {
-	type="splash";
+        type="splashheal";
 }
 
-void Priest::fightAll(vector<MilitaryUnit*>& Allies)
-{	
-	int Alive=0;
-	
-	for(int i=0;i<Allies.size();++i)
-	{
-		if(Allies[i]->isAlive()==true) ++Alive;
-	}
+void Priest::fightAll(vector<MilitaryUnit*>& targets)
+{        
+        int Alive=0;
+        
+        for(int i=0;i<targets.size();++i)
+        {
+                if(targets[i]->isAlive()==true) ++Alive;
+        }
 
-	int heal=(attackDamage/Alive);
-	
-	for(int i=0;i<Allies.size();++i)
-	{
-		if(Allies[i]->isAlive()==true) Allies[i]->receiveHealth(heal);
-	}
+        int heal=(attackDamage/Alive);
+        
+        for(int i=0;i<targets.size();++i)
+        {
+                if(targets[i]->isAlive()==true) 
+				{
+					targets[i]->receiveHealth(heal);
+					cout << name << " has healed " << targets[i]->getName() << " for " << heal << endl;
+				}
+        }
 }
