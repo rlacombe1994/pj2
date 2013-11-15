2b1d7a27de5a8022eb0aa212290d2743cfddcb4a
mseeg <Canismajoris@gmx.com>
2013-11-15 13:45:33 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -35,88 +35,13 @@ void gameIntro()
 	basicRules();
  }
  
- void addUnit(vector<MilitaryUnit*>& playersSquad, char classType, int numberOfUnits)
- {
-	string name;
-	for(int i =0; i<numberOfUnits; ++i)
-	{
-		switch(classType)
-		{
-			case 'A':
-				cout<<"what do you wish to name your Assassin?\t\t";
-				cin>>name;
-				playersSquad.push_back(new Assassin(name,10));
-				break;
-			case 'B':
-				cout<<"what do you wish to name your BladeMaster?\t";
-				cin>>name;
-				playersSquad.push_back(new BladeMaster(name,10));
-				break;
-			case 'M':
-				cout<<"what do you wish to name your Monk?\t\t";
-				cin>>name;
-				playersSquad.push_back(new Monk(name,10));
-				break;
-			case 'N':
-				cout<<"what do you wish to name your Necromancer?\t";
-				cin>>name;
-				playersSquad.push_back(new Necromancer(name,10));
-				break;
-			case 'T':
-				cout<<"what do you wish to name your Tank?\t\t";
-				cin>>name;
-				playersSquad.push_back(new Tank(name,10));
-				break;	
-		}
-	}
-	return;
- }
- 
- vector<MilitaryUnit*> buildSquad()
- {
-	vector<MilitaryUnit*> playersSquad;
-	int Assassins =0;
-	int BladeMasters =0;
-	int Monks =0;
-	int Necromancers =0;
-	int Tanks =0;
-	int totalUnits =0;
-	while(totalUnits != 5)
-	{
-		cout<<"How many of each unit will you take to battle(maximum of 5)?"<<endl;
-		cout<<"Assassins:";
-		cin>>Assassins;
-		cout<<"BladeMasters:";
-		cin>>BladeMasters;
-		cout<<"Monks:";
-		cin>>Monks;
-		cout<<"Necromancers:";
-		cin>>Necromancers;
-		cout<<"Tanks:";
-		cin>>Tanks;
-		
-		totalUnits = Assassins + BladeMasters + Monks + Necromancers + Tanks;
-	}
-	cout<<endl;
-	addUnit(playersSquad,'A',Assassins);
-	addUnit(playersSquad,'B',BladeMasters);
-	addUnit(playersSquad,'M',Monks);
-	addUnit(playersSquad,'N',Necromancers);
-	addUnit(playersSquad,'T',Tanks);
-	
-	return playersSquad;
- }
- 
 int main()
  {
 	gameIntro();
-    DefenseOfTheCodeGame game = DefenseOfTheCodeGame();
-	buildSquad();
+    	DefenseOfTheCodeGame game = DefenseOfTheCodeGame();
 	
-	/*
-    game.run();
+    	game.run();
 
-    printWinner(game);
-	*/
-    return 0;
+    	printWinner(game);
+    	return 0;
 }
