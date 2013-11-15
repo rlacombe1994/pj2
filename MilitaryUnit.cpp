2a1ca6704187ece3bfaf9722636e0a169abe3fc3
rlacombe1994 <rlacombe1994@hotmail.com>
2013-11-15 15:30:34 -0600

    Update MilitaryUnit.cpp

--- MilitaryUnit.cpp
+++ MilitaryUnit.cpp
@@ -7,10 +7,17 @@ MilitaryUnit::MilitaryUnit(string name, int attackDamage) {
         this->attackDamage = attackDamage;
         armorDefense = 5;
         health = 100;
+		type = "solo";
 };
 
-void MilitaryUnit::receiveDamage(int damage) {
+void MilitaryUnit::receiveDamage(int damage) 
+{
         health -= (damage - armorDefense);
+}
+
+void MilitaryUnit::receiveHealth(int heal)
+{
+        health += heal;
 }
 
 string MilitaryUnit::getName() { return name; }
@@ -18,21 +25,10 @@ string MilitaryUnit::getName() { return name; }
 string MilitaryUnit::getType() { return type; }
 
 int MilitaryUnit::getHealth() { return health; }
-
-int MilitaryUnit::getAttack(){ return attackDamage; }
-
-int MilitaryUnit::getArmor() { return armorDefense; } 
-
-void MilitaryUnit::setHealth(int health){ this->health = health;} 
 		
 void MilitaryUnit::fight(MilitaryUnit *opponent) {
         opponent->receiveDamage(attackDamage);
 };
-
-void MilitaryUnit::receiveHealth(int heal)
-{
-	health += heal;
-}
 
 bool MilitaryUnit::isAlive() {
         return this->health > 0;
